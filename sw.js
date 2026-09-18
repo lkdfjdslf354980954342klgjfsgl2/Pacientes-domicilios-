16a4d23ffaff0ffd38737b8be388deda76addd8d
Ser <128261262+lkdfjdslf354980954342klgjfsgl2@users.noreply.github.com>
2026-09-17 23:22:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,23 @@
-const CACHE = 'panel-pacientes-v1';
+const CACHE = 'prexa-v11-13-final';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(['./panel-pacientes.html']))
+    caches.open(CACHE).then((cache) => cache.addAll([
+      './panel-pacientes.html',
+      './manifest.json',
+      './icon-192.png',
+      './icon-512.png'
+    ]))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
   event.waitUntil(self.clients.claim());
 });
 
