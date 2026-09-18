ad641870c1fa2c1af9c88b3c2951ce24518d7572
Ser <128261262+lkdfjdslf354980954342klgjfsgl2@users.noreply.github.com>
2026-09-18 17:51:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,4 @@
-const CACHE = 'prexa-v11-13-final';
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll([
-      './panel-pacientes.html',
-      './manifest.json',
-      './icon-192.png',
-      './icon-512.png'
-    ]))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    ))
-  );
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(()=> self.clients.claim()));
 });
